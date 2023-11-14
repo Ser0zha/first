c20baebd1ce58430e57cdc0c37040045ab9b0081
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2023-11-15 02:22:37 +0300

    Update 8-3.py

--- 8-3.py
+++ 8-3.py
@@ -1,5 +1,5 @@
 Translate = {"Привет": "Hello", "Мир": "World", "Я": "I'm", "Машина": "Car", "Магия": "Magic", "Существо": "Creature"}
 Inp = input("Введите строку: ").split(" ")
-Inp = [i.capitalize() for i in inp]
-Out = [translate.get(i.capitalize()) if translate.get(i) else i for i in inp]
-print("Перевод:\n", *out, sep='')
+Inp = [i.capitalize() for i in Inp]
+Out = [translate.get(i.capitalize()) if translate.get(i) else i for i in Inp]
+print("Перевод:\n", *Out, sep='')
