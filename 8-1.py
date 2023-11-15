2df2dc8de90795896a4e48ae62f2ac9417266d57
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2023-11-15 22:18:03 +0300

    Update 8-1.py

--- 8-1.py
+++ 8-1.py
@@ -1,14 +1,14 @@
-dictionary = {"Ivanov": 20, "Sidorov": 68, "Petrov": 26, "Smirnov": 68}
+Dictionary = {"Ivanov": 20, "Sidorov": 68, "Petrov": 26, "Smirnov": 68}
 k = ''
-dictionary["Kolchin"] = 80
-dictionary["Krivov"] = 70
-m = max(dictionary.values())
-mm = min(dictionary.values())
-avg = sum(dictionary.values()) / len(dictionary)
-maxi = [i for i in dictionary if dictionary.get(i) == m]
-mini = [i for i in dictionary if dictionary.get(i) == mm]
-for i in dictionary:
-    if dictionary[i] > avg:
+Dictionary["Kolchin"] = 80
+Dictionary["Krivov"] = 70
+m = max(Dictionary.values())
+mm = min(Dictionary.values())
+avg = sum(Dictionary.values()) / len(Dictionary)
+maxi = [i for i in Dictionary if Dictionary.get(i) == m]
+mini = [i for i in Dictionary if Dictionary.get(i) == mm]
+for i in Dictionary:
+    if Dictionary[i] > avg:
         k += i + " "
 print(f"Студенты баллы которых выше среднего: \n{k}\n")
 print(f"Минимальный балл: {mm} ", *mini, sep="")
