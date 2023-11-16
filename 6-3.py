e817f1a0faa4e1a566151214794407d0312ac290
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2023-11-16 16:09:39 +0300

    Update 6-3.py

--- 6-3.py
+++ 6-3.py
@@ -1,8 +1,6 @@
 from collections import Counter
-print("Введите массив:")
-arr = [str(i) for i in input().split()]
+arr = input("Введите массив:").split()
 first = Counter(arr).most_common()[0][0]
-second = Counter(arr).most_common()[1][0]
 if Counter(arr).most_common()[0][1] != Counter(arr).most_common()[1][1]:
     print("Мода массива:", first)
 else:
